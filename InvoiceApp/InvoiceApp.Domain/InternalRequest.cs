3389fda95da3b47b190f166c62c1b9205149f51b
unknown <cainezekiel456@gmail.com>
2022-12-25 16:58:35 +0200

    Adding Internal Request Domain Project

---
+++ InvoiceApp/InvoiceApp.Domain/InternalRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.Domain
+{
+    public class InternalRequest
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Project { get; set; }
+    }
+}
